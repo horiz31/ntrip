c59b4978f307735dea5ce011b0b9b7f2c54d1642
Bogdan Vacaliuc <bvacaliuc@gmail.com>
2020-10-13 13:05:30 -0400

    perform the install steps in the postinst section of the update script; this is to let swu work right

--- meta-ntrip/recipes-support/swupdate/ntrip-swu/ntrip-update.sh
+++ meta-ntrip/recipes-support/swupdate/ntrip-swu/ntrip-update.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+S=/usr/local/src/ntrip
+D=
+systemd_unitdir=/lib/systemd
 
 if [ $# -lt 1 ]; then
 	exit 0;
@@ -6,14 +10,24 @@ fi
 
 if [ $1 == "preinst" ] ; then
 	# must exist, else swupdate will fail
-	mkdir -p /usr/local/src/ntrip
+	mkdir -p ${S}
 	exit 0
 fi
 
 if [ $1 == "postinst" ] ; then
-	rm -f /usr/local/src/ntrip/ntrip-application.tar.gz
-	make -C /usr/local/src/ntrip install
+	# remove tarball from upload
+	rm -f ${S}/ntrip-application.tar.gz
+	# put config files in the needed places
+	install -m 0644 ${S}/config/gpsd.conf ${D}/etc/systemd
+	install -m 0644 ${S}/config/mavproxy.conf ${D}/etc/systemd
+	install -m 0644 ${S}/config/network.conf ${D}/etc/systemd
+	install -m 0644 ${S}/config/ntpd.conf ${D}/etc/systemd
+	install -m 0644 ${S}/config/etc-ntp.conf ${D}/etc/ntp.conf
+	install -m 0644 ${S}/ensure-network.service ${D}${systemd_unitdir}/system/ensure-network.service
+	install -m 0644 ${S}/mavproxy.service ${D}${systemd_unitdir}/system/mavproxy.service
+	systemctl daemon-reload
+	systemctl enable ensure-network mavproxy
+	# ensure the FMU uart is in the right mode for mavlink I/O
 	stty -F /dev/ttymxc3 cs8 -parenb -cstopb
-	/usr/local/src/ntrip/ensure-network.sh
 	exit 0
 fi
